6782388ce952e691f348884359a420d26fc32607
Hilman <maulana.mclb@gmail.com>
2020-01-09 13:23:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World War III Iran vs USA!");
+    response.end("Hello World War III Iran vs USA we are waiting it to staaphhh!");
 });
 
 const port = process.env.PORT || 1337;
